1efd97565f80c7bce18d16a904f51d270a3323e3
Ashor Esho <ashorashor132123@gmail.com>
2022-06-08 01:39:07 +0000

    Just peace

---
+++ bot.lua
@@ -0,0 +1,124 @@
+local token = '5323542329:AAHqfZ6NfsHvSWhyZIcTcwCu_Cn3WUA8phQ'
+local admin = "709733712";
+local bot = require('telegram-bot-lua.core').configure(token)
+local tools = require('telegram-bot-lua.tools')
+local redis = require 'redis'
+local thread = require "llthreads2"
+local json = require("dkjson")
+local client = redis.connect('127.0.0.1', 6379)
+local rq = require("requests")
+local mn = {}
+local startTime = os.time()
+
+local function stopFile(name, text)
+    local file = io.open("stops/stop[" .. name .. "].txt", "w")
+    file:write(text)
+    file:close()
+end
+
+function time_format(seconds)
+    local seconds = tonumber(seconds)
+    local d = math.floor(seconds / (3600 * 24))
+    local h = math.floor(seconds / 3600 % 24)
+    local m = math.floor(seconds % 3600 / 60)
+    local s = math.floor(seconds % 3600 % 60)
+    if d > 0 then
+        return ('%.fD %.fH %.fm %.fs'):format(d, h, m, s)
+    elseif h > 0 then
+        return ('%.fH %.fm %.fs'):format(h, m, s)
+    elseif m > 0 then
+        return ('%.fm %.fs'):format(m, s)
+    elseif s > 0 then
+        return ('%.fs'):format(s)
+    end
+    return '-'
+end
+
+function bot.on_update(update)
+    if not update.message then return end
+    local message = update.message
+    local chat_id = message.chat.id
+    local from_id = tostring(message.from.id)
+    local first_name = message.from.first_name
+    local text = message.text
+    local chat_type = message.chat.type
+    if (not client:sismember("totalusers", from_id)) and chat_type == "private" then
+        client:sadd("totalusers", from_id)
+        print("new user: " .. first_name .. "\nid: " .. from_id)
+    end
+    if (not client:sismember("totalsupergroups", chat_id)) and chat_type == "supergroup" then
+        client:sadd("totalsupergroups", chat_id)
+        print("new group: " .. message.chat.title .. "\nid: " .. chat_id)
+        bot.send_message(admin, "New group !\n" .. message.chat.title .. "\n@" .. (message.chat.username or "None"))
+    end
+    if text and text:match("^/start") then
+        return bot.send_message(chat_id, "Hello, *" .. tools.escape_markdown(first_name) .. "*\nuse `/mention` to mention all users.\n- *`Note`*:\n1- it only works on supergroups\n2- you need to be an Admin for this to work.\n3- theBot does not require to be An admin for this to work.",
+            "markdown",
+            true, false, message.message_id, bot.inline_keyboard():row(
+                bot.row():url_button(
+                    "🧚‍♂️",
+                    'https://t.me/notmafia')))
+    end
+    if text and text:match("^/mention") and chat_type == "supergroup" then
+        local stats = rq.get("https://api.telegram.org/bot" .. token .. "/getChatMember?chat_id=" .. chat_id .. "&user_id=" .. from_id).text;
+        local info = json.decode(stats);
+        local you = info['result']['status'];
+        if (string.lower(you) == "administrator") or (string.lower(you) == "creator" or from_id == admin) then
+            if client:get("mn:" .. chat_id) then return end
+            bot.send_message(chat_id, "Okay, *" .. tools.escape_markdown(first_name) .. "*\n`Note:`\n1- It send's 4 users each 1 second\n2- It can't send over 4 users per message, telegram issues.\n3- have fun :)", "markdown",
+                true, false, message.message_id, bot.inline_keyboard():row(
+                    bot.row():url_button(
+                        "🧚‍♂️",
+                        'https://t.me/notmafia')))
+            local tt = string.format([[
+chat_id = "%s"
+redis = require("redis")
+local client = redis.connect('127.0.0.1', 6379)
+os.execute("python3 mention.py "..chat_id)
+client:del("mn:"..chat_id)
+client:del("stop:"..chat_id)
+]]           , chat_id)
+            mn[chat_id] = thread.new(tt)
+            mn[chat_id]:start()
+            client:rpush("totalmentions", 1)
+            return client:set("mn:" .. chat_id, 1)
+        else
+            return bot.send_message(chat_id, "You're not an Admin!", "markdown",
+                true, false, message.message_id)
+        end
+    end
+    --
+    --
+    --
+    --
+    --
+    if text and text:match("^/stopmention") and chat_type == "supergroup" then
+        local stats = rq.get("https://api.telegram.org/bot" .. token .. "/getChatMember?chat_id=" .. chat_id .. "&user_id=" .. from_id).text;
+        local info = json.decode(stats);
+        local you = info['result']['status'];
+        if (string.lower(you) == "administrator") or (string.lower(you) == "creator" or from_id == admin) then
+            if not client:get("mn:" .. chat_id) then
+                return bot.send_message(chat_id, "*No running mentions!*", "markdown",
+                    true, false, message.message_id)
+            end
+            bot.send_message(chat_id, "Okay, *" .. tools.escape_markdown(first_name) .. "*\nStopping now.", "markdown",
+                true, false, message.message_id, bot.inline_keyboard():row(
+                    bot.row():url_button(
+                        "🧚‍♂️",
+                        'https://t.me/notmafia')))
+            mn[chat_id] = nil;
+            client:rpush("totalstopmentions", 1)
+            return client:set("stop:" .. chat_id, 1)
+        else
+            return bot.send_message(chat_id, "You're not an Admin!", "markdown",
+                true, false, message.message_id)
+        end
+    end
+    if text and text:match("^/stats") then
+        return bot.send_message(chat_id,
+            "*Up time*: `" .. time_format(os.difftime(os.time(), startTime)) .. "`\n\n- Users: *" .. (#client:smembers("totalusers") or 0) .. "*\n- Groups: *" .. (#client:smembers("totalsupergroups") or 0) .. "*\n- Mentions: *" .. (#(client:lrange("totalmentions", 0, -1) or 0)) .. "*\n- Stoped Mentions: *" .. (#(client:lrange("totalstopmentions", 0, -1) or 0)) .. "*", "markdown",
+            true, false, message.message_id)
+    end
+end
+
+bot.run()
---
+++ mention.py
@@ -0,0 +1,73 @@
+"""
+Made by NotMafia.
+Telegram: @NotMafia
+Github: NotMafia1010
+Version: 1.0
+License: MIT
+Copyright (c) 2020 NotMafia, All rights reserved.
+This code is under MIT license!
+You can use this code in your project, but you can't claim it as your own.
+If you want to use this code in your project, you must provide a link to this repository.
+Enjoy :)
+"""
+token = "5323542329:AAHqfZ6NfsHvSWhyZIcTcwCu_Cn3WUA8phQ"  # Your bot token.
+api_id = 15942537  # the api id from my.telegram.org
+api_hash = "e891c773ab993957f8e94c8d62a32fa1"  # the api hash from my.telegram.org
+
+try:
+    import asyncio, sys, time, redis
+    from telethon import TelegramClient
+except ImportError:
+    print("Please install the required libraries. (pip install -r requirements.txt)")
+    sys.exit()
+
+r = redis.StrictRedis(
+    host="localhost", port=6379, charset="utf-8", decode_responses=True
+)
+try:
+    chat_id = int(sys.argv[1])  # the chatID of the group you want to mention.
+except IndexError:  # if no chatID is given.
+    print("Usage: python3 mention.py <chat_id>")
+    sys.exit()
+storeMentionsWithString, StoreMentionsArray = "", []
+overFourMentionsSum, totalUsers = [0, 0]
+
+
+async def starts():  # start the bot function
+    global storeMentionsWithString, overFourMentionsSum, totalUsers
+    try:
+        app = TelegramClient(
+            token.split(":")[0], api_id, api_hash
+        )  # Connect to the client.
+        await app.connect()  # Connect to the client.
+        if not await app.is_user_authorized():  # Check if the user is authorized.
+            await app.start(bot_token=token)  # if not then Start the client.
+        for user in await app.get_participants(chat_id):  # Get all participants.
+            if user.id and user.first_name:
+                StoreMentionsArray.append(
+                    f"[{user.first_name}](tg://user?id={user.id})"
+                )
+        for mention in StoreMentionsArray:  # Get all mentions.
+            if r.get(f"stop:{chat_id}"):  # Check if the bot is stopped.
+                print("stopped: " + str(chat_id))
+                return sys.exit()
+            overFourMentionsSum += 1
+            totalUsers += 1
+            storeMentionsWithString += str(totalUsers) + "- " + str(mention) + "\n"
+            if overFourMentionsSum == 4:
+                await app.send_message(chat_id, storeMentionsWithString)
+                storeMentionsWithString, overFourMentionsSum = "", 0
+                time.sleep(
+                    1
+                )  # sleep for 1 second to prevent telegram from blocking the bot.
+        await app.send_message(chat_id, "Done!,\ntotal users ~ " + str(totalUsers))
+    except Exception as e:  # If there is an error.
+        with open("log.txt", "a") as f:  # Log the error.
+            f.write(str(e) + "\n")
+        f.close()
+    print("done: " + str(chat_id))  # print the chat id that the bot finished.
+
+
+if __name__ == "__main__":  # start the bot.
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(starts())
